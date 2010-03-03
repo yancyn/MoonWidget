bebdc03d9d6a3ceb67db8525b71ee8b7601bc8f2
yancyn <yancyn@e76a38c8-1c55-11df-a59c-8573ac22324a>
2010-03-03 12:40:25 +0000

    create a different test method instead of just keep every single testing block of code at Program.cs.

--- Snippet/Program.cs
+++ Snippet/Program.cs
@@ -1,9 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Data;
 using System.Linq;
 using System.IO;
+using System.Text;
+using System.Globalization;
 
 namespace Snippet
 {
